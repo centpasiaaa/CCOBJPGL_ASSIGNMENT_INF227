34aadacf9836e01b1e986299f3fab3bdd187977d
Vincent Nathaniel A. Pasia <130419400+centpasiaaa@users.noreply.github.com>
2023-05-15 22:46:45 +0800

    exercise3
    exercise3

---
+++ Cat/Birman.java
@@ -0,0 +1,8 @@
+package Cat;
+interface Birman {
+
+    String eyeColor = "blue";
+    String height = "15 inches";
+
+    void meow();
+}
---
+++ Cat/Dog/Pomeranian.java
@@ -0,0 +1,8 @@
+package Cat.Dog;
+
+interface Pomeranian {
+
+    String height = "15 inches";
+
+    void bark();
+}
