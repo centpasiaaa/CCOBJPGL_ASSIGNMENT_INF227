dca60a8df006a1f1cf766dbb21210469f43e3279
Vincent Nathaniel A. Pasia <130419400+centpasiaaa@users.noreply.github.com>
2023-05-23 12:10:59 +0800

    exercise5

--- src/App.java
+++ src/App.java
@@ -1,28 +1,27 @@
-import Explorers.HumanExplorer;
-import Explorers.*;
-import Planets.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class App extends Application {
 
-public class App {
     public static void main(String[] args) {
-        
-        Planet mars = new Mars();
-        Planet saturn = new Saturn();
-        Planet mercury = new Mercury();
-        Planet earth = new Earth();
 
-
-        Explorer astronaut = new HumanExplorer();
-        Explorer rover = new RobotExplorer();
-
-        mars.accept(astronaut);
-        saturn.accept(astronaut);
-        mercury.accept(astronaut);
-        earth.accept(astronaut);
-
-        mars.accept(rover);
-        saturn.accept(rover);
-        mercury.accept(rover);
-        earth.accept(rover);
+        launch(args);
     }
+
+    public void start(Stage stage) throws Exception {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/Restaurant.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
